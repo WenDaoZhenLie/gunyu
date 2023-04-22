1ee6ff5743abb7166200bc14a4f23cbadc7bd1a7
WenDaoZhenLie <grezhenweiliu@icloud.com>
2023-04-23 01:49:12 +0800

    init

---
+++ gunyu-common/src/main/java/pers/wendaozhenlie/gunyucommon/GunyuCommonApplication.java
@@ -0,0 +1,13 @@
+package pers.wendaozhenlie.gunyucommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunyuCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunyuCommonApplication.class, args);
+    }
+
+}
---
+++ gunyu-registry/src/test/java/pers/wendaozhenlie/gunyuregistry/GunyuRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.wendaozhenlie.gunyuregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunyuRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
