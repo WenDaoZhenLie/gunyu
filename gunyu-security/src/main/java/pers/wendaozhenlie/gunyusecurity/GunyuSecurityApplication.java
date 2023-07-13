639d9a30ca264896787c175355881abc963f37e9
WenDaoZhenLie <grezhenweiliu@icloud.com>
2023-07-14 01:18:12 +0800

    add security module

---
+++ gunyu-security/src/main/java/pers/wendaozhenlie/gunyusecurity/GunyuSecurityApplication.java
@@ -0,0 +1,13 @@
+package pers.wendaozhenlie.gunyusecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunyuSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunyuSecurityApplication.class, args);
+    }
+
+}
